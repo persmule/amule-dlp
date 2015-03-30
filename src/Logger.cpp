49284122cf4fc1d8863ad96fe01338a0d097def1
persmule <persmule@gmail.com>
2015-03-30 23:28:11 +0800

    Add a log file: Records for antileech will be write to $HOME/.aMule/antileech.log.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -305,6 +305,8 @@ void CLogger::FlushApplog()
 }
 
 CLogger theLogger;
+//Dynamic Leech Protect - persmule
+CLogger dlpLogger;
 
 BEGIN_EVENT_TABLE(CLogger, wxEvtHandler)
 	EVT_MULE_LOGGING(CLogger::OnLoggingEvent)
